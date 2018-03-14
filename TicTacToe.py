556e23f68b91308b70d3dcfe5394ce0da3587a2a
Diego Vargas <diegofvargas@gmail.com>
2018-03-14 00:50:10 -0400

    functional game that checks rows and columns for winner

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,42 +1,49 @@
 test_board = ['#','X','O','X','O','X','O','X','O','X']
 board = ['#','','','','','','','','','']
-game_on = True
+
 def print_board():
     print(board[1]+'|'+board[2]+'|'+board[3])
     print(board[4]+'|'+board[5]+'|'+board[6])
     print(board[7]+'|'+board[8]+'|'+board[9])
 
 def run_game():
+    game_on = True
     print('Welcome to TicTacToe')
-    print_board()
     while(game_on):
+        print_board()
         print('Player One, what is your move?')
-        move1 = input('Please select a cell to play')
-        check_played(move1,one)
+        move1 = int(input('Please select a cell to play'))
+        check_played(move1,'one')
+        game_on = check_winner()
+        print_board()
         print('Player Two, what is your move?')
-        move2 = input('Please select a cell to play')
-        check_played(move2,two)
-        game_on=False
+        move2 = int(input('Please select a cell to play'))
+        check_played(move2,'two')
+        game_on = check_winner()
 
 def check_winner():
     for row in range(1,10,3):
-        if board[row]==board[row+1] and board[row]==board[row+2]:
-            if board[row]='X':
+        if board[row]==board[row+1] and board[row]==board[row+2] and board[row]!='':
+            if board[row]=='X':
                 print('Player one wins')
-            elif boad[row]='O':
+            elif board[row]=='O':
                 print('Player two wins')
+            return False
     for column in range(1,4):
-        if board[column]==board[column+3] and board[column]==board[column+6]:
-            if board[column]='X':
+        if board[column]==board[column+3] and board[column]==board[column+6] and board[column]!='':
+            if board[column]=='X':
                 print('Player one wins')
-            elif boad[column]='O':
+            elif board[column]=='O':
                 print('Player two wins')
+            return False
+    return True
 
 def check_played(move,player):
-    if board[move]!='':
-        input('Cell has been used, please enter a different one')
-    else:
-        if player=='one':
-            board[move]='X'
-        elif player=='two':
-            board[move]='O'
+    while board[move]!='':
+        move = int(input('Cell has been used, please enter a different one'))
+        
+    if player=='one':
+        board[move]='X'
+    elif player=='two':
+        board[move]='O'
+run_game()
